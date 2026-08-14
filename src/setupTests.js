49dba6f82ca883161afdb14eed36cddf4cd1d3a2
rishabh <rishabhguptalahar@gmail.com>
2026-08-15 02:04:49 +0530

    test: suppress act() warning for async auth thunk in tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+// Suppress act() warning for async thunk dispatch in App.js
+const originalError = console.error;
+console.error = (...args) => {
+  if (typeof args[0] === "string" && args[0].includes("not wrapped in act")) {
+    return;
+  }
+  originalError.call(console, ...args);
+};
