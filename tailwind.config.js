9cddece7c273be1835478dbbd1cc919218726feb
rishabh <rishabhguptalahar@gmail.com>
2026-08-27 00:33:55 +0530

    Fix - Dependency bloat

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import withMT from "@material-tailwind/html/utils/withMT";
+import withMT from "@material-tailwind/react/utils/withMT";
 export default withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
